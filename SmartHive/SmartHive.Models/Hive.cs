30c1c0fa76d4ce947f96752d56965810815c5ca2
Branimir <brani_svetoslavov@abv.bg>
2017-05-19 19:07:21 +0300

    Add models project and base models

---
+++ SmartHive/SmartHive.Models/Hive.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartHive.Models
+{
+    public class Hive
+    {
+        public Hive()
+        {
+
+        }
+
+        public Hive(string name, string dataKey)
+        {
+            this.Name = name;
+            this.DataKey = dataKey;
+        }
+
+        [Key]
+        public int HiveId { get; set; }
+
+        public string Name { get; set; }
+
+        public string DataKey { get; set; }
+
+    }
+}
---
+++ SmartHive/SmartHive.Models/User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SmartHive.Models
+{
+    public class User : IdentityUser
+    {
+        private ICollection<Hive> Hives;
+
+        public User() : base(string.Empty)
+        {
+
+        }
+
+        public User(string username, string email, string name, string description, string profilePicUrl)
+        {
+            this.UserName = username;
+            this.Email = email;
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public virtual ICollection<Hive> HiveCollection
+        {
+            get { return this.Hives; }
+            set { this.Hives = value; }
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            return manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        }
+    }
+}
