65ef3619aa3fa4e9d3b7cd4827e2e4568912fc97
Branimir <brani_svetoslavov@abv.bg>
2017-05-23 15:26:32 +0300

    Add factories

---
+++ SmartHive/SmartHive.Factories/IHiveFactory.cs
@@ -0,0 +1,9 @@
+﻿using SmartHive.Models;
+
+namespace SmartHive.Factories
+{
+    public interface IHiveFactory
+    {
+        Hive CreateHive(string name, string dataKey);
+    }
+}
---
+++ SmartHive/SmartHive.Factories/IUserFactory.cs
@@ -0,0 +1,9 @@
+﻿using SmartHive.Models;
+
+namespace SmartHive.Factories
+{
+    public interface IUserFactory
+    {
+        User CreateUser(string username, string email, string name, string description);
+    }
+}
