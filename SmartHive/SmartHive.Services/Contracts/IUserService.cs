9265100121579365010ef52e59be8876f59b1329
Branimir <brani_svetoslavov@abv.bg>
2017-05-23 15:32:17 +0300

    Add services project and implemented UserService

---
+++ SmartHive/SmartHive.Services/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using SmartHive.Models;
+using System.Collections.Generic;
+
+namespace SmartHive.Services.Contracts
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetUsers();
+
+        User GetUserById(string id);
+
+        User GetUserByUsername(string username);
+    }
+}
---
+++ SmartHive/SmartHive.Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using SmartHive.Data.Contracts;
+using SmartHive.Models;
+using SmartHive.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartHive.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> userRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserService(
+            IRepository<User> userRepository,
+            IUnitOfWork unitOfWork)
+        {
+            if (userRepository == null)
+            {
+                throw new ArgumentNullException("userRepository");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWorks");
+            }
+
+            this.userRepository = userRepository;
+            this.unitOfWork = unitOfWork;
+        }
+        public IEnumerable<User> GetUsers()
+        {
+            return this.userRepository.GetAll
+                .ToList();
+        }
+
+        public User GetUserById(string id)
+        {
+            return this.userRepository.GetById(id);
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            return this.userRepository.GetAll.FirstOrDefault(u => u.UserName.Equals(username));
+        }
+
+        public void EditUser(string id, string username, string name, string description)
+        {
+            var user = this.userRepository.GetById(id);
+
+            if (user != null)
+            {
+                user.UserName = username;
+                user.Name = name;
+                user.Description = description;
+
+                this.userRepository.Update(user);
+                this.unitOfWork.Commit();
+            }
+        }
+    }
+}
