7b39ac025f348634d21efa303fb2fc2a5c76cd09
Branimir <brani_svetoslavov@abv.bg>
2017-05-25 13:54:23 +0300

    Add GetCurrentUserId test

---
+++ SmartHive/SmartHive.Authentication.Tests/HttpContextAuthenticationProvider/GetCurrentUserId_Should.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using SmartHive.Providers.Contracts;
+using System.Security.Principal;
+
+namespace SmartHive.Authentication.Tests.HttpContextAuthenticationProviderTests
+{
+    [TestFixture]
+    public class GetCurrentUserId_Should
+    {
+        [Test]
+        public void _Call_HttpContextProviderCurrentIdentity()
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedIdentity = new Mock<IIdentity>();
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(provider => provider.CurrentIdentity).Returns(mockedIdentity.Object);
+
+            var httpContextAuthProvider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object,
+                mockedDateTimeProvider.Object);
+
+            //Act
+            var result = httpContextAuthProvider.CurrentUserId;
+
+            //Assert
+            mockedHttpContextProvider.Verify(provider => provider.CurrentIdentity, Times.Once);
+        }
+    }
+}
