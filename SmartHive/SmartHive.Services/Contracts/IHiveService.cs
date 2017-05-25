238148c40f424022fc6ca8211393a06b03452a06
Branimir <brani_svetoslavov@abv.bg>
2017-05-25 17:08:46 +0300

    Add IHiveService interface

---
+++ SmartHive/SmartHive.Services/Contracts/IHiveService.cs
@@ -0,0 +1,14 @@
+﻿using SmartHive.Models;
+using System.Collections.Generic;
+
+namespace SmartHive.Services.Contracts
+{
+    public interface IHiveService
+    {
+        Hive GetHiveById(string id);
+
+        IEnumerable<Hive> GetHivesByUsername(string username);
+
+        IEnumerable<Hive> GetHivesByUser(User user);
+    }
+}
