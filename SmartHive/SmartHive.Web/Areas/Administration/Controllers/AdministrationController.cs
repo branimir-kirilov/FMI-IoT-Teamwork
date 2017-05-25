3678ea131f909fc22e1b1586239868350a298358
Branimir <brani_svetoslavov@abv.bg>
2017-05-25 13:02:04 +0300

    Add administration area

---
+++ SmartHive/SmartHive.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SmartHive.Web.Areas.Administration.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Administration/Administration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
