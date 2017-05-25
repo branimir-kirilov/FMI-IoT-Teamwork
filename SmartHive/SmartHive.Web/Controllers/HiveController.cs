3388f3df1a9f1c4b1303f448164a0244b1866bba
Branimir <brani_svetoslavov@abv.bg>
2017-05-25 18:29:42 +0300

    Add Hive Controllerand fix csproj

---
+++ SmartHive/SmartHive.Web/Controllers/HiveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartHive.Web.Controllers
+{
+    public class HiveController : Controller
+    {
+        // GET: Hive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
