cf95082bbe0743db044a12c869ac7c3a9aff0b49
Branimir <brani_svetoslavov@abv.bg>
2017-05-25 14:06:20 +0300

    Add RegisterAndLoginUser tests

---
+++ SmartHive/SmartHive.Authentication.Tests/HttpContextAuthenticationProvider/RegisterAndLoginUser_Should.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNet.Identity;
+using Moq;
+using NUnit.Framework;
+using SmartHive.Authentication.ApplicationManagers;
+using SmartHive.Models;
+using SmartHive.Providers.Contracts;
+using Microsoft.Owin.Security;
+
+namespace SmartHive.Authentication.Tests.HttpContextAuthenticationProviderTests
+{
+    [TestFixture]
+    public class RegisterAndLoginUser_Should
+    {
+        [TestCase("fakePassword", true, true)]
+        [TestCase("fakePassword", false, false)]
+        [TestCase("fakePassword", true, false)]
+        [TestCase("fakePassword", false, true)]
+        public void _Call_UserManager_Create_Method(string password, bool isPersistant, bool rememberMe)
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedUserStore = new Mock<IUserStore<User>>();
+
+            var mockedUserManager = new Mock<ApplicationUserManager>(mockedUserStore.Object);
+            mockedUserManager.Setup(man => man.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(new IdentityResult());
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.GetUserManager<ApplicationUserManager>())
+                .Returns(mockedUserManager.Object);
+
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object, mockedDateTimeProvider.Object);
+
+            var user = new User();
+
+            //Act
+            provider.RegisterAndLoginUser(user, password, isPersistant, rememberMe);
+
+            //Assert
+            mockedUserManager.Verify(m => m.CreateAsync(user, password), Times.Once);
+        }
+
+
+        [TestCase("fakePassword", true, true)]
+        [TestCase("fakePassword", false, false)]
+        [TestCase("fakePassword", true, false)]
+        [TestCase("fakePassword", false, true)]
+        public void _Call_SignInManager_SignIn_Method(string password, bool isPersistant, bool rememberMe)
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedUserStore = new Mock<IUserStore<User>>();
+            var mockedUserManager = new Mock<ApplicationUserManager>(mockedUserStore.Object);
+            mockedUserManager.Setup(man => man.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+
+            var mockedAuthenticationManager = new Mock<IAuthenticationManager>();
+            var mockedSignInManager = new Mock<ApplicationSignInManager>(
+                mockedUserManager.Object,
+                mockedAuthenticationManager.Object);
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.GetUserManager<ApplicationUserManager>())
+                .Returns(mockedUserManager.Object);
+            mockedHttpContextProvider.Setup(p => p.GetUserManager<ApplicationSignInManager>())
+                .Returns(mockedSignInManager.Object);
+
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object, mockedDateTimeProvider.Object);
+
+            var user = new User();
+
+            //Act
+            provider.RegisterAndLoginUser(user, password, isPersistant, rememberMe);
+
+            //Assert
+            mockedSignInManager.Verify(m => m.SignInAsync(user, isPersistant, rememberMe), Times.Once);
+        }
+
+        [TestCase("fakePassword", true, true)]
+        [TestCase("fakePassword", false, false)]
+        [TestCase("fakePassword", true, false)]
+        [TestCase("fakePassword", false, true)]
+        public void _Return_IdentityResult(string password, bool isPersistant, bool rememberMe)
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var expected = new IdentityResult();
+
+            var mockedUserStore = new Mock<IUserStore<User>>();
+            var mockedUserManager = new Mock<ApplicationUserManager>(mockedUserStore.Object);
+            mockedUserManager.Setup(man => man.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(expected);
+
+
+            var mockedAuthenticationManager = new Mock<IAuthenticationManager>();
+            var mockedSignInManager = new Mock<ApplicationSignInManager>(
+                mockedUserManager.Object,
+                mockedAuthenticationManager.Object);
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.GetUserManager<ApplicationUserManager>())
+                .Returns(mockedUserManager.Object);
+            mockedHttpContextProvider.Setup(p => p.GetUserManager<ApplicationSignInManager>())
+                .Returns(mockedSignInManager.Object);
+
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object, mockedDateTimeProvider.Object);
+
+            var user = new User();
+
+            //Act
+            var result = provider.RegisterAndLoginUser(user, password, isPersistant, rememberMe);
+
+            //Assert
+            Assert.AreSame(expected, result);
+
+        }
+    }
+}
