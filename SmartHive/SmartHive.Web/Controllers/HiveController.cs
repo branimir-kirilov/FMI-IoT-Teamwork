22fc5309439b4f3aeedd118cdf532a738074e86a
Branimir <brani_svetoslavov@abv.bg>
2017-05-30 16:08:59 +0300

    Hie controller authorize

--- SmartHive/SmartHive.Web/Controllers/HiveController.cs
+++ SmartHive/SmartHive.Web/Controllers/HiveController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SmartHive.Web.Controllers
 {
+    [Authorize]
     public class HiveController : Controller
     {
         // GET: Hive
