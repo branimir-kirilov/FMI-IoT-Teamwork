2bd1595c897edd1d637580b71111517214ac0cfc
Branimir <brani_svetoslavov@abv.bg>
2017-05-31 00:16:04 +0300

    Introduced profile page

--- SmartHive/SmartHive.Web/App_Start/RouteConfig.cs
+++ SmartHive/SmartHive.Web/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace SmartHive.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+              name: "UserDetails",
+              url: "Profile/Details/{username}",
+              defaults: new { controller = "Profile", action = "UserProfile" }
+          );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
