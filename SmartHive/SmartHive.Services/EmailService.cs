a85780b2f46e4e4fd809bf7256f797b76d8750c3
dragomirevgeniev <dragomirevgeniev@gmail.com>
2017-05-30 13:59:03 +0300

    adding email and sms notification services

---
+++ SmartHive/SmartHive.Services/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Text;
+using System.Diagnostics;
+
+namespace SmartHive.Services
+{
+    public class EmailService
+    {
+        public async Task SendAsync(string sendTo, string subject, string body)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 465;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("smarthivebg@gmail.com", "123456smart");
+            MailMessage message = new MailMessage("donotreply@domain.com", sendTo, subject, body);
+            message.BodyEncoding = UTF8Encoding.UTF8;
+            message.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            if (client != null)
+            {
+                await client.SendMailAsync(message);
+            }
+            else
+            {
+                Trace.TraceError("Failed to create a connection with smtp.");
+                await Task.FromResult(0);
+            }
+        }
+    }
+}
---
+++ SmartHive/SmartHive.Services/SMSSenderService.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace SmartHive.Services
+{
+    public class SMSSenderService
+    {
+        private readonly string accountSid = "ACe1a461fd80cabc103810f2786edc6b97";
+        private readonly string authToken = "5ebf770fda1fe337396ee6c2961da113n";
+        private readonly string companyNumber = "+359898739493";
+
+        public async Task SendAsync(string sendTo, string text)
+        {
+            TwilioRestClient twilio = new TwilioRestClient(accountSid, authToken);
+
+            if (twilio != null)
+            {
+               twilio.SendMessage(companyNumber, sendTo, text);
+            }
+            else
+            {
+                Trace.TraceError("Failed to create a connection with twilio.");
+                await Task.FromResult(0);
+            }
+        }
+    }
+}
